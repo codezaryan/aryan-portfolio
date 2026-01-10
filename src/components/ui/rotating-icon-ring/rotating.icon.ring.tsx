f0e34a2c40732e18eb98b95a7d710db90a657942
Ajay malah <arryroy1101@gmail.com>
2026-01-11 01:09:15 +0800

    26.10.1

--- src/components/ui/rotating-icon-ring/rotating.icon.ring.tsx
+++ src/components/ui/rotating-icon-ring/rotating.icon.ring.tsx
@@ -1,77 +1,69 @@
 'use client';
 
 import React from 'react';
-
 import UserAvatar from './widgets/user.avatar';
 import IconRing, { IconAndNames } from './widgets/icon.ring';
 import DecorativeRing from './widgets/decorative.rings';
-import { SiExpress, SiHibernate, SiMongodb, SiNodedotjs, SiReact, SiSpringboot } from 'react-icons/si';
+import {
+  SiExpress,
+  SiHibernate,
+  SiMongodb,
+  SiMysql,
+  SiNodedotjs,
+  SiReact,
+  SiSpringboot
+} from 'react-icons/si';
 import { useRotation } from '@/lib/hooks/useRoatation';
+import { useMediaQuery } from '@/lib/hooks/useMediaQuery';
 import { HeroImages } from '@/lib/constants/images/hero/hero.images';
 
+const RotatingIconsRing: React.FC = () => {
+  const rotation = useRotation(15);
 
-interface RotatingIconsRingProps {
-  userImage?: string;
-  innerRadius?: number;
-  outerRadius?: number;
-  innerIcons?: IconAndNames[];
-  outerIcons?: IconAndNames[];
-  rotationSpeed?: number;
-  iconSize?: number;
-}
+  const isLg = useMediaQuery('(min-width: 1024px)');
+  const isXl = useMediaQuery('(min-width: 1280px)');
 
-const RotatingIconsRing: React.FC<RotatingIconsRingProps> = ({ 
-  innerRadius = 160,
-  outerRadius = 210,
-  innerIcons = [
-    { icon: SiSpringboot, name: "Spring Boot", color: "#6DB33F" },
-    { icon: SiHibernate, name: "Hibernate", color: "#59666C" },
-    { icon: SiMongodb, name: "MongoDB", color: "#47A248" },
-    { icon: SiMongodb, name: "MongoDB", color: "#47A248" },
-    { icon: SiMongodb, name: "MongoDB", color: "#47A248" }
-  ],
-  outerIcons = [
-    { icon: SiMongodb, name: "MongoDB", color: "#47A248" },
-    { icon: SiExpress, name: "Express.js", color: "#000000" },
-    { icon: SiReact, name: "React", color: "#61DAFB" },
-    { icon: SiNodedotjs, name: "Node.js", color: "#339933" },
-    { icon: SiMongodb, name: "MongoDB", color: "#47A248" },
-    { icon: SiMongodb, name: "MongoDB", color: "#47A248" }
-  ],
-  rotationSpeed = 15,
-  iconSize = 36
-}) => {
-  const rotation = useRotation(rotationSpeed);
+  const innerRadius = isXl ? 200 : isLg ? 170 : 120;
+  const outerRadius = isXl ? 270 : isLg ? 230 : 170;
+  const iconSize = isXl ? 44 : isLg ? 40 : 32;
 
   return (
-    <div className="flex items-center justify-center min-h-screen relative pt-20 pb-28 px-6 overflow-hidden">
-      <div 
-        className="relative" 
-        style={{ 
-          width: outerRadius * 2 + 100, 
-          height: outerRadius * 2 + 100 
+    <div className="flex items-center justify-center relative overflow-hidden">
+      <div
+        className="relative"
+        style={{
+          width: outerRadius * 2 + 100,
+          height: outerRadius * 2 + 100
         }}
       >
-        <IconRing 
-          icons={innerIcons}
+        <IconRing
+          icons={[
+            { icon: SiSpringboot, name: 'Spring Boot', color: '#6DB33F' },
+            { icon: SiHibernate, name: 'Hibernate', color: '#6DB33F' },
+            { icon: SiMongodb, name: 'MongoDB', color: '#47A248' },
+
+          ]}
           radius={innerRadius}
           rotation={rotation}
-          reverse={false}
           iconSize={iconSize}
-          className="z-10"
         />
 
         <UserAvatar imageUrl={HeroImages.my_avatar} maxRadius={outerRadius} />
-        
-        <IconRing 
-          icons={outerIcons}
+
+        <IconRing
+          icons={[
+             {icon: SiMysql ,name:"MySQL",color:"#61DAFB"},
+            { icon: SiExpress, name: 'Express.js', color: '#47A248' },
+            { icon: SiReact, name: 'React', color: '#61DAFB' },
+            { icon: SiNodedotjs, name: 'Node.js', color: '#339933' }
+          ]}
           radius={outerRadius}
           rotation={rotation}
-          reverse={true}
-          iconSize={iconSize}
+          reverse
+          iconSize={iconSize*1.5}
           className='z-10'
         />
-        
+
         <DecorativeRing dashCount={50} radius={innerRadius} />
         <DecorativeRing dashWidth={5} radius={outerRadius} />
       </div>
